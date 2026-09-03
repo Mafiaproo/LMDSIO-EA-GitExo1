5c88c4fbc313c06d4b5ee301f78c368ec6ec959d
Enzo ANDRE <eandr@lmdsio.com>
2026-09-03 14:28:38 +0200

    Mon projet v2

--- accueil.php
+++ accueil.php
@@ -1,9 +1,22 @@
 <?php
 
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']));
+
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    $nom = htmlspecialchars($_POST['nom']);
-    $prenom = htmlspecialchars($_POST['prenom']);
+    if($_POST['login'] && $_POST['password'])
+    {
+        $login = htmlspecialchars($_POST['login']);
+        $pass = $_POST['password'];
+        if($login == "admin" && $pass == "azerty")
+        {
 
-    echo "Bonjour $nom $prenom bienvenue sur votre page.";
+            header("Location: http://$host$uri/profile.html");
+        }
+        else
+        {
+            header("Location: http://$host$uri/index.html");
+        }
+    }
 }
